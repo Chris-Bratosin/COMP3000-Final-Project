de4df38aea586f248fc7e2486de78ca91181a87b
Chris Bratosin <chris.bratosin@students.plymouth.ac.uk>
2026-05-07 07:42:55 +0100

    chore: cleaned up and deleted old database models

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,11 +1,3 @@
 module.exports = {
-  AuditLog: require('./AuditLog'),
-  AuthSession: require('./AuthSession'),
-  AwsConnection: require('./AwsConnection'),
-  Finding: require('./Finding'),
-  Report: require('./Report'),
-  ScanPolicy: require('./ScanPolicy'),
-  ScanRun: require('./ScanRun'),
-  User: require('./User'),
-  Workspace: require('./Workspace'),
+  ScanRecord: require('./ScanRecord'),
 };
